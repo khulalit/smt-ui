e11c89a2178d7ddbc8d829b0571fbdbedf24ac62
LalitKhu <lalitkhudhania2@gmail.com>
2026-05-27 18:30:50 +0530

    feat: add navbar component with nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import { Navbar } from './components/Navbar';
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-slate-50 text-slate-800 antialiased flex flex-col">
+        <Navbar />
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<Navigate to="/insights" replace />} />
